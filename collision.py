54ca7bf2be7f841efa7ec0f806c39f11340119d5
Mathew <ameliasolder@mail.ru>
2025-12-16 21:10:21 +0300

    Task 4 complete

--- collision.py
+++ collision.py
@@ -17,3 +17,25 @@ def isCollisionRect(rect1, rect2):
         return False
     else:
         return True
+
+def intersectionAreaRect(rect1, rect2):
+    if not isCorrectRect(rect1):
+        raise ValueError("1й прямоугольник некорректный")
+    if not isCorrectRect(rect2):
+        raise ValueError("2й прямоугольник некорректный")
+    
+    x1_min, y1_min = rect1[0]
+    x1_max, y1_max = rect1[1]
+    x2_min, y2_min = rect2[0]
+    x2_max, y2_max = rect2[1]
+    
+    x_left = max(x1_min, x2_min)
+    x_right = min(x1_max, x2_max)
+    y_bottom = max(y1_min, y2_min)
+    y_top = min(y1_max, y2_max)
+    
+    if x_left < x_right and y_bottom < y_top:
+        area = (x_right - x_left) * (y_top - y_bottom)
+        return area
+    else:
+        return 0
